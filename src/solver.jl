7402eea8cac1dc25bfbc04a70bc466f2f85d1279
Thomas Kemmer <thomas@binaryden.de>
2020-02-12 15:07:18 +0100

    Prepare matvec for an actual vector x

--- src/solver.jl
+++ src/solver.jl
@@ -8,13 +8,23 @@ function solve(
     Ξ        = CuArray(unpack([e.center for e in model.elements]))
     numelem  = length(model.elements)
 
-    matvec(elements, Ξ, CuArrays.ones(T, numelem), numelem, model.params)
+    matvec(
+        elements,
+        Ξ,
+        CuArrays.ones(T, numelem),
+        CuArrays.ones(T, numelem),
+        CuArrays.ones(T, numelem),
+        numelem,
+        model.params
+    )
 end
 
 function matvec(
     elements::CuVector{T},
     Ξ       ::CuVector{T},
-    x       ::CuVector{T},
+    x1      ::CuVector{T},
+    x2      ::CuVector{T},
+    x3      ::CuVector{T},
     numelem ::Int,
     params  ::Option{T}
 ) where T
@@ -22,26 +32,26 @@ function matvec(
     _config(kernel) = (threads = 256, blocks = cld(numelem, 256))
 
     v11 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec11!(v11, elements, Ξ, x, numelem, yuk)
+    @cuda config=_config matvec11!(v11, elements, Ξ, x1, numelem, yuk)
 
     v12 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec12!(v12, elements, Ξ, x, numelem,
+    @cuda config=_config matvec12!(v12, elements, Ξ, x2, numelem,
         params.εΩ * (1/params.ε∞ - 1/params.εΣ), params.εΩ/params.ε∞, yuk)
 
     v13 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec13!(v13, elements, Ξ, x, numelem, params.ε∞/params.εΣ, yuk)
+    @cuda config=_config matvec13!(v13, elements, Ξ, x3, numelem, params.ε∞/params.εΣ, yuk)
 
     v21 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec21!(v21, elements, Ξ, x, numelem)
+    @cuda config=_config matvec21!(v21, elements, Ξ, x1, numelem)
 
     v22 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec22!(v22, elements, Ξ, x, numelem)
+    @cuda config=_config matvec22!(v22, elements, Ξ, x2, numelem)
 
     v32 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec32!(v32, elements, Ξ, x, numelem, params.εΩ/params.ε∞)
+    @cuda config=_config matvec32!(v32, elements, Ξ, x2, numelem, params.εΩ/params.ε∞)
 
     v33 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec33!(v33, elements, Ξ, x, numelem)
+    @cuda config=_config matvec33!(v33, elements, Ξ, x3, numelem)
 
     [v11 .+ v12 .+ v13; v21 .+ v22; v32 .+ v33]
 end

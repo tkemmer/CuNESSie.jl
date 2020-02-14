6af2b9fbb36c80c681b5231b7566765477279fc4
Thomas Kemmer <thomas@binaryden.de>
2020-02-14 13:12:20 +0100

    Save some gpu memory

--- src/matrix.jl
+++ src/matrix.jl
@@ -20,10 +20,7 @@ end
 ) where T = error("setindex! not defined for ", typeof(A))
 
 @inline function Base.:*(A::SystemMatrix{T}, x::AbstractArray{T, 1}) where T
-    x1 = CuArray(x[1:A.numelem])
-    x2 = CuArray(x[(A.numelem + 1):(2 * A.numelem)])
-    x3 = CuArray(x[(2 * A.numelem + 1):(3 * A.numelem)])
-    Array(matvec(A.elements, A.Ξ, x1, x2, x3, A.numelem, A.params))
+    Array(matvec(A.elements, A.Ξ, x, A.numelem, A.params))
 end
 
 # TODO mul!/5
@@ -38,36 +35,33 @@ end
 function matvec(
     elements::CuVector{T},
     Ξ       ::CuVector{T},
-    x1      ::CuVector{T},
-    x2      ::CuVector{T},
-    x3      ::CuVector{T},
+    x       ::AbstractArray{T, 1},
     numelem ::Int,
     params  ::Option{T}
 ) where T
     yuk = yukawa(params)
     _config(kernel) = (threads = 256, blocks = cld(numelem, 256))
 
+    cux = CuArray(x[1:numelem])
     v11 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec11!(v11, elements, Ξ, x1, numelem, yuk)
-
-    v12 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec12!(v12, elements, Ξ, x2, numelem,
-        params.εΩ * (1/params.ε∞ - 1/params.εΣ), params.εΩ/params.ε∞, yuk)
-
-    v13 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec13!(v13, elements, Ξ, x3, numelem, params.ε∞/params.εΣ, yuk)
-
     v21 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec21!(v21, elements, Ξ, x1, numelem)
+    @cuda config=_config matvec11!(v11, elements, Ξ, cux, numelem, yuk)
+    @cuda config=_config matvec21!(v21, elements, Ξ, cux, numelem)
 
+    cux = CuArray(x[(numelem+1):(2numelem)])
+    v12 = CuArray{T}(undef, numelem)
     v22 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec22!(v22, elements, Ξ, x2, numelem)
-
     v32 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec32!(v32, elements, Ξ, x2, numelem, params.εΩ/params.ε∞)
+    @cuda config=_config matvec12!(v12, elements, Ξ, cux, numelem,
+        params.εΩ * (1/params.ε∞ - 1/params.εΣ), params.εΩ/params.ε∞, yuk)
+    @cuda config=_config matvec22!(v22, elements, Ξ, cux, numelem)
+    @cuda config=_config matvec32!(v32, elements, Ξ, cux, numelem, params.εΩ/params.ε∞)
 
+    cux = CuArray(x[(2numelem+1):(3numelem)])
+    v13 = CuArray{T}(undef, numelem)
     v33 = CuArray{T}(undef, numelem)
-    @cuda config=_config matvec33!(v33, elements, Ξ, x3, numelem)
+    @cuda config=_config matvec13!(v13, elements, Ξ, cux, numelem, params.ε∞/params.εΣ, yuk)
+    @cuda config=_config matvec33!(v33, elements, Ξ, cux, numelem)
 
     [v11 .+ v12 .+ v13; v21 .+ v22; v32 .+ v33]
 end

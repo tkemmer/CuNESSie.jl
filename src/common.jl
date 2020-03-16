8ccc91e09385f3ee2cfdfc382cebbb7238f27bce
Thomas Kemmer <thomas@binaryden.de>
2020-03-16 14:52:40 +0100

    common: add more _etol overloads

--- src/common.jl
+++ src/common.jl
@@ -1,2 +1,7 @@
-@inline _etol(::Float64) = 1.45e-8
-@inline _etol(::Float32) = 3.45e-4
+const _etol_f64 = 1.45e-8
+const _etol_f32 = 3.45e-4
+
+@inline _etol(::Float64) = _etol_f64
+@inline _etol(::Type{Float64}) = _etol_f64
+@inline _etol(::Float32) = _etol_f32
+@inline _etol(::Type{Float32}) = _etol_f32

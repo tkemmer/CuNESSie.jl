2e0a74c7f35d46e1f7c57c1764b65f1069ffb994
Thomas Kemmer <thomas@binaryden.de>
2020-03-16 13:48:58 +0100

    nonlocal: add NonlocalSystemOutputs type

--- src/nonlocal/rhs.jl
+++ src/nonlocal/rhs.jl
@@ -1,3 +1,48 @@
+# TODO
+struct NonlocalSystemOutputs{T} <: AbstractArray{T, 1}
+    β      ::Vector{T}
+    numelem::Int
+end
+
+function NonlocalSystemOutputs(
+    A   ::NonlocalSystemMatrix{T},
+    umol::Vector{T},
+    qmol::Vector{T}
+) where T
+    yuk = yukawa(A.params)
+    _config(kernel) = (threads = 128, blocks = cld(A.numelem, 128))
+
+    cux = CuArray(umol)
+    ks  = CuArray{T}(undef, A.numelem)
+    @cuda config=_config _rhs_k_kernel!(ks, A.elements, A.Ξ, cux, A.numelem,
+        one(yuk) - (A.params.εΩ / A.params.εΣ), yuk)
+
+    cux .= CuArray(qmol)
+    vs  = CuArray{T}(undef, A.numelem)
+    @cuda config=_config _rhs_v_kernel!(vs, A.elements, A.Ξ, cux, A.numelem,
+        A.params.εΩ * (one(yuk)/A.params.εΣ - one(yuk)/A.params.ε∞),
+        A.params.εΩ / A.params.ε∞,
+        yuk
+    )
+
+    NonlocalSystemOutputs(Array(ks .+ vs), A.numelem)
+end
+
+@inline Base.size(v::NonlocalSystemOutputs{T}) where T = (3 * v.numelem, )
+
+@inline Base.getindex(
+    v::NonlocalSystemOutputs{T},
+    i::Int
+) where T = i ∈ (v.numelem+1):(3*v.numelem) ? zero(T) : Base.getindex(v.β, i)
+# FIXME checkounds message
+
+@inline Base.setindex(
+    v::NonlocalSystemOutputs{T},
+     ::Any,
+     ::Int
+) where T = error("setindex! not defined for ", typeof(v))
+
+# deprecated
 function righthandside(
     Ξ       ::CuVector{T},
     elements::CuVector{T},

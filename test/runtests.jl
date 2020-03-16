c6af74306e5254ece4e523735eddbe27d6f1b37f
Thomas Kemmer <thomas@binaryden.de>
2020-03-16 16:03:04 +0100

    test: restructure dirs according to src

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,9 +8,14 @@ using Test
 using CuNESSie: Ξ2device, elements2device
 
 @testset "CuNESSie.jl" begin
-    @testset "math.jl" begin include("math.jl") end
-    @testset "laplace.jl" begin include("laplace.jl") end
-    @testset "yukawa.jl" begin include("yukawa.jl") end
-    @testset "matrix.jl" begin include("matrix.jl") end
-    @testset "rhs.jl" begin include("rhs.jl") end
+    @testset "base" begin
+        @testset "math.jl" begin include("base/math.jl") end
+        @testset "laplace.jl" begin include("base/laplace.jl") end
+        @testset "yukawa.jl" begin include("base/yukawa.jl") end
+    end
+
+    @testset "nonlocal" begin
+        @testset "matrix.jl" begin include("nonlocal/matrix.jl") end
+        @testset "rhs.jl" begin include("nonlocal/rhs.jl") end
+    end
 end

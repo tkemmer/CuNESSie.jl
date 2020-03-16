2a929ad3557351faf52eb249fb601167fc860e24
Thomas Kemmer <thomas@binaryden.de>
2020-03-16 16:48:55 +0100

    nonlocal: have solve() return NonlocalBEMResults

--- src/nonlocal/solver.jl
+++ src/nonlocal/solver.jl
@@ -18,11 +18,24 @@ function NonlocalSystem(model::Model{T, Triangle{T}}) where T
     NonlocalSystem(model, A, b, umol, qmol)
 end
 
-function solve(sys::NonlocalSystem{T}) where T
+function _solve(sys::NonlocalSystem{T}) where T
     gmres(sys.A, sys.b,
         verbose=true,
         restart=min(200, size(sys.A, 2)),
         Pl=DiagonalPreconditioner(sys.A)
+    )
+end
+
+function solve(sys::NonlocalSystem{T}) where T
+    numelem = sys.A.numelem
+    cauchy  = _solve(sys)
+    NESSie.BEM.NonlocalBEMResult(
+        sys.model,
+        view(cauchy, 1:numelem),
+        view(cauchy, 1+numelem:2numelem),
+        view(cauchy, 1+2numelem:3numelem),
+        sys.umol,
+        sys.qmol
     )
 end
 

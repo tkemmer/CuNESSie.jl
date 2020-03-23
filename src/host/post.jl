0da7b3a23fc634ad94024e08b2e65e813a1bbc9d
Thomas Kemmer <thomas@binaryden.de>
2020-03-23 16:23:27 +0100

    post: add local/nonlocal reaction field potential φ*Ω

--- src/host/post.jl
+++ src/host/post.jl
@@ -7,33 +7,33 @@ end
     PostProcessor(bem, TriangleVector(bem.model.elements))
 end
 
-function rfenergy(proc::PostProcessor{T}) where T
-    cuΞ   = PositionVector([c.pos for c in proc.bem.model.charges])
-    qvals = [c.val for c in proc.bem.model.charges]
+function φ⃰Ω(Ξ::Vector{Vector{T}}, proc::PostProcessor{T}) where T
+    Ξ = PositionVector(Ξ)
+    Ṽ = LaplacePotentialMatrix{SingleLayer}(Ξ, proc.elements)
+    W = LaplacePotentialMatrix{DoubleLayer}(Ξ, proc.elements)
 
-    Ṽ  = LaplacePotentialMatrix{SingleLayer}(cuΞ, proc.elements)
-    W  = LaplacePotentialMatrix{DoubleLayer}(cuΞ, proc.elements)
-
-    (Ṽ * proc.bem.q .- W * proc.bem.u) ⋅ qvals * T(55.27935812569474)
+    (NESSie.potprefactor(T) / T(4π)) .* (Ṽ * proc.bem.q .- W * proc.bem.u)
 end
 
-@inline rfenergy(
-    bem::R
-) where {T, R <: NESSie.BEM.BEMResult{T}} = rfenergy(PostProcessor(bem))
-
 function φΩ(Ξ::Vector{Vector{T}}, proc::PostProcessor{T}) where T
-    cuΞ = PositionVector(Ξ)
+    εΩ = proc.bem.model.params.εΩ
     φm  = φmol(Ξ, proc.bem.model.charges)
 
-    Ṽ = LaplacePotentialMatrix{SingleLayer}(cuΞ, proc.elements)
-    W = LaplacePotentialMatrix{DoubleLayer}(cuΞ, proc.elements)
-
-    εΩ = proc.bem.model.params.εΩ
-    NESSie.potprefactor(T) .*
-        (T(4π) .\ (Ṽ * proc.bem.q .- W * proc.bem.u) .+ εΩ .\ φm)
+    φ⃰Ω(Ξ, proc) .+ (NESSie.potprefactor(T) / εΩ .* φm)
 end
 
 @inline φΩ(
     Ξ  ::Vector{Vector{T}},
     bem::R
 ) where {T, R <: NESSie.BEM.BEMResult{T}} = φΩ(Ξ, PostProcessor(bem))
+
+function rfenergy(proc::PostProcessor{T}) where T
+    qposs = [c.pos for c in proc.bem.model.charges]
+    qvals = [c.val for c in proc.bem.model.charges]
+
+    φ⃰Ω(qposs, proc) ⋅ qvals * T(48.242647748524156)
+end
+
+@inline rfenergy(
+    bem::R
+) where {T, R <: NESSie.BEM.BEMResult{T}} = rfenergy(PostProcessor(bem))

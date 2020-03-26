5bd9d6f5b2f32092e3726d057592101d0144c9b9
Thomas Kemmer <thomas@binaryden.de>
2020-03-26 10:56:42 +0100

    matrix: generate similar functions

--- src/device/matrix.jl
+++ src/device/matrix.jl
@@ -11,17 +11,15 @@ function _laplace_diag_kernel!(
     nothing
 end
 
-@inline _laplace_single_diag_kernel!(
-    dst     ::CuDeviceVector{T},
-    Ξ       ::CuPositionVector{T},
-    elements::CuTriangleVector{T}
-) where T = _laplace_diag_kernel!(dst, Ξ, elements, laplacepot_single)
-
-@inline _laplace_double_diag_kernel!(
-    dst     ::CuDeviceVector{T},
-    Ξ       ::CuPositionVector{T},
-    elements::CuTriangleVector{T}
-) where T = _laplace_diag_kernel!(dst, Ξ, elements, laplacepot_double)
+for L in [:single, :double]
+    @eval begin
+        @inline $(Symbol("_laplace_", L, "_diag_kernel!"))(
+            dst     ::CuDeviceVector{T},
+            Ξ       ::CuPositionVector{T},
+            elements::CuTriangleVector{T}
+        ) where T = _laplace_diag_kernel!(dst, Ξ, elements, $(Symbol("laplacepot_", L)))
+    end
+end
 
 function _laplace_mul_kernel!(
     dst     ::CuDeviceVector{T},
@@ -42,19 +40,16 @@ function _laplace_mul_kernel!(
     nothing
 end
 
-@inline _laplace_single_mul_kernel!(
-    dst     ::CuDeviceVector{T},
-    Ξ       ::CuPositionVector{T},
-    elements::CuTriangleVector{T},
-    x       ::CuDeviceVector{T}
-) where T = _laplace_mul_kernel!(dst, Ξ, elements, x, laplacepot_single)
-
-@inline _laplace_double_mul_kernel!(
-    dst     ::CuDeviceVector{T},
-    Ξ       ::CuPositionVector{T},
-    elements::CuTriangleVector{T},
-    x       ::CuDeviceVector{T}
-) where T = _laplace_mul_kernel!(dst, Ξ, elements, x, laplacepot_double)
+for L in [:single, :double]
+    @eval begin
+        @inline $(Symbol("_laplace_", L, "_mul_kernel!"))(
+            dst     ::CuDeviceVector{T},
+            Ξ       ::CuPositionVector{T},
+            elements::CuTriangleVector{T},
+            x       ::CuDeviceVector{T}
+        ) where T = _laplace_mul_kernel!(dst, Ξ, elements, x, $(Symbol("laplacepot_", L)))
+    end
+end
 
 function _reyukawa_diag_kernel!(
     dst     ::CuDeviceVector{T},
@@ -70,19 +65,16 @@ function _reyukawa_diag_kernel!(
     nothing
 end
 
-@inline _reyukawa_single_diag_kernel!(
-    dst     ::CuDeviceVector{T},
-    Ξ       ::CuPositionVector{T},
-    elements::CuTriangleVector{T},
-    yuk     ::T
-) where T = _reyukawa_diag_kernel!(dst, Ξ, elements, yuk, regularyukawapot_single)
-
-@inline _reyukawa_double_diag_kernel!(
-    dst     ::CuDeviceVector{T},
-    Ξ       ::CuPositionVector{T},
-    elements::CuTriangleVector{T},
-    yuk     ::T
-) where T = _reyukawa_diag_kernel!(dst, Ξ, elements, yuk, regularyukawapot_double)
+for L in [:single, :double]
+    @eval begin
+        @inline $(Symbol("_reyukawa_", L, "_diag_kernel!"))(
+            dst     ::CuDeviceVector{T},
+            Ξ       ::CuPositionVector{T},
+            elements::CuTriangleVector{T},
+            yuk     ::T
+        ) where T = _reyukawa_diag_kernel!(dst, Ξ, elements, yuk, $(Symbol("regularyukawapot_", L)))
+    end
+end
 
 function _reyukawa_mul_kernel!(
     dst     ::CuDeviceVector{T},
@@ -104,18 +96,16 @@ function _reyukawa_mul_kernel!(
     nothing
 end
 
-@inline _reyukawa_single_mul_kernel!(
-    dst     ::CuDeviceVector{T},
-    Ξ       ::CuPositionVector{T},
-    elements::CuTriangleVector{T},
-    x       ::CuDeviceVector{T},
-    yuk     ::T
-) where T = _reyukawa_mul_kernel!(dst, Ξ, elements, x, yuk, regularyukawapot_single)
-
-@inline _reyukawa_double_mul_kernel!(
-    dst     ::CuDeviceVector{T},
-    Ξ       ::CuPositionVector{T},
-    elements::CuTriangleVector{T},
-    x       ::CuDeviceVector{T},
-    yuk     ::T
-) where T = _reyukawa_mul_kernel!(dst, Ξ, elements, x, yuk, regularyukawapot_double)
+for L in [:single, :double]
+    @eval begin
+        @inline $(Symbol("_reyukawa_", L, "_mul_kernel!"))(
+            dst     ::CuDeviceVector{T},
+            Ξ       ::CuPositionVector{T},
+            elements::CuTriangleVector{T},
+            x       ::CuDeviceVector{T},
+            yuk     ::T
+        ) where T = _reyukawa_mul_kernel!(
+            dst, Ξ, elements, x, yuk, $(Symbol("regularyukawapot_", L))
+        )
+    end
+end

19051f26aa4f0bb73da3304c7a0351d0fc9f9f52
Thomas Kemmer <thomas@binaryden.de>
2020-03-26 12:26:39 +0100

    common: add abstract array types for common implementations

--- src/host/common.jl
+++ src/host/common.jl
@@ -9,6 +9,20 @@ const _etol_f32 = 3.45f-4
 @inline _kcfg(n::Int) = _ -> (threads = 128, blocks = cld(n, 128))
 @inline _kcfg(A::PotentialMatrix{T}) where T = _kcfg(size(A, 1))
 
+abstract type ReadOnlyArray{T, N} <: AbstractArray{T, N} end
+abstract type NoAccessArray{T, N} <: AbstractArray{T, N} end
+
+@inline Base.getindex(A::NoAccessArray, ::Int) = error("getindex not defined for ", typeof(A))
+
+@inline Base.setindex!(
+    A::Union{ReadOnlyArray, NoAccessArray},
+     ::Any,
+     ::Int
+) = error("setindex! not defined for ", typeof(A))
+
+@inline Base.show(io::IO, ::MIME"text/plain", A ::NoAccessArray) = Base.show(io, A)
+@inline Base.show(io::IO, A::NoAccessArray) = print(io, join(size(A), "×"), " ", typeof(A))
+
 function _solve_linear_system(A::AbstractArray{T, 2}, b::AbstractArray{T, 1}) where T
     gmres(A, b,
         verbose=true,

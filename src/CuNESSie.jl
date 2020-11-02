1bb96cd8463db29a25829a702023590888afd863
Thomas Kemmer <thomas@binaryden.de>
2020-11-02 21:30:30 +0100

    Only export solvers/post-processors compatible w/ NESSie
    We might reenable the export of CuNESSie-specific types/functions in the
    future.

--- src/CuNESSie.jl
+++ src/CuNESSie.jl
@@ -17,8 +17,8 @@ include("device/matrix.jl")
 include("device/laplace.jl")
 include("device/yukawa.jl")
 include("device/nonlocal.jl")
-export CuPosition, CuPositionVector, CuTriangle, CuTriangleVector, NoAccessArray, ReadOnlyArray,
-    laplacepot_single, laplacepot_double, regularyukawapot_single, regularyukawapot_double
+#export CuPosition, CuPositionVector, CuTriangle, CuTriangleVector, NoAccessArray, ReadOnlyArray,
+#    laplacepot_single, laplacepot_double, regularyukawapot_single, regularyukawapot_double
 
 # host code
 include("host/common.jl")
@@ -27,8 +27,9 @@ include("host/matrix.jl")
 include("host/local.jl")
 include("host/nonlocal.jl")
 include("host/post.jl")
-export LaplacePotentialMatrix, LocalSystem, LocalSystemMatrix, NonlocalSystem,
-    NonlocalSystemMatrix, NonlocalSystemOutputs, PositionVector, PostProcessor,
-    ReYukawaPotentialMatrix, TriangleVector, rfenergy, solve, φ⃰Ω, φΩ, φΣ
+#export LaplacePotentialMatrix, LocalSystem, LocalSystemMatrix, NonlocalSystem,
+#    NonlocalSystemMatrix, NonlocalSystemOutputs, PositionVector, PostProcessor,
+#    ReYukawaPotentialMatrix, TriangleVector, φ⃰Ω
+export rfenergy, solve, φΩ, φΣ
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,14 @@
-using CuNESSie
 using CuArrays
 using CUDAnative
 using NESSie
 using ImplicitArrays
 using Test
+
+using CuNESSie
+using CuNESSie: CuPosition, CuPositionVector, CuTriangle, CuTriangleVector, PositionVector,
+    TriangleVector, laplacepot_single, laplacepot_double, regularyukawapot_single,
+    regularyukawapot_double
+
 
 @testset "CuNESSie.jl" begin
     @testset "device" begin

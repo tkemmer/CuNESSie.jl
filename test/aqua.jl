fa63efacc2300e0bb0d8b8ce82a888d05dcc8dc4
Thomas Kemmer <thomas@binaryden.de>
2024-10-16 13:11:11 +0200

    Bump Aqua version, remove workaround

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,14 +1,7 @@
 @testitem "Aqua" begin
     using Aqua
 
-    # Current workaround to avoid failing tests due to ambiguity in dependencies
-    # https://github.com/JuliaTesting/Aqua.jl/issues/77
-    @testset "Method ambiguity" begin
-        Aqua.test_ambiguities(CuNESSie)
-    end
-
     Aqua.test_all(CuNESSie;
-        ambiguities=false,
         persistent_tasks=false # requires NESSie to be registered
     )
 end
